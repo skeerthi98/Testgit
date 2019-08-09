d6997ab9ac06ee6e3110a63d9a2c8b78b07bb1f6
skeerthi98 <37667607+skeerthi98@users.noreply.github.com>
2019-08-09 06:12:12 +0530

    Date component commited

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { User } from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,5 @@ import { User } from './address-card/user.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user:User;
-  inputText:string="Initial Value";
- constructor()
- {
-   this.user = new User();
-   this.user.name="Keerthi";
-   this.user.title="software Engineer";
-   this.user.address="gandhinagar,vanasthalipuram";
-   this.user.Phone = [ '8074579249',
-     '7032794006'
-
-   ];
- }
+  title = 'First project';
 }
--- app.module.ts
+++ app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddressCardComponent } from './address-card/address-card.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddressCardComponent
+    HelloWorldComponent,
+    DateComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
